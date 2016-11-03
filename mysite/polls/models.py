bea4eedc5b26b91e6dbac87c08024a7478caa93f
Roddy Dairion <roddy@orange.mu>
2016-11-03 23:15:22 +0400

    Comiit

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Capture(models.Model):
+	base_language=models.Charfield(max_length=25)
+	company_name=models.Charfield(max_length=35)
+	id=models.AutoField(primary_key=True)
